b8676f63e13895bf1436117f3175e115f0030ac8
Michael Propp <michaelpropp8664@gmail.com>
2019-07-07 13:11:54 +0200

    Another commit

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express =require('express');
 const app = express();
+const PORT = process.env.PORT || "3000";
 app.get('/', (req,res) => {
     res.send("Home Page");
 });
-app.listen(3000)
-console.log("Running")
+app.listen(PORT, () => console.log("listening"));
 //type "heroku auth:login"
 //"heroku create" must have git 
 //git push heroku master"
